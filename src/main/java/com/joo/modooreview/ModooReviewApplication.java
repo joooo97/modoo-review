5c2e1d90f3f27829df5033c7e849be4c9e7b9ae7
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-12-27 02:12:39 +0900

    Initial commit

---
+++ src/main/java/com/joo/modooreview/ModooReviewApplication.java
@@ -0,0 +1,13 @@
+package com.joo.modooreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModooReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModooReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joo/modooreview/ModooReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.modooreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModooReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
